35dadabc456ec1046572f32214c5f33f7806c70c
Victor Chin <junk@objectmage.com>
2017-02-04 13:19:14 +0100

    active-directory-dotnet-native-desktop

---
+++ active-directory-dotnet-native-desktop/TodoListService/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="modernizr-2.7.2.js" />
+/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="jquery-1.10.2.js" />
+/// <reference path="jquery-2.1.1.js" />
+/// <reference path="respond.js" />
+/// <reference path="respond.matchmedia.addlistener.js" />
